044f3034891fdbf3f6df29cffa099039d031c341
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 13:44:05 -0800

    master generating file  in action shared with caller

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,12 @@ working_directory=$2
 echo "docker pwd $(pwd)"
 echo "workflow workspace $working_directory"
 
+touch hello-world.txt
+file="$workding_directory/hello-world.txt"
+
 echo "Hello $who_to_greet"
 time=$(date)
 tag=refs/tags/1.1.0-rc.3
 echo ::set-output name=time::$time
 echo ::set-output name=os::$OSTYPE
-echo ::set-output name=tag::$OSTYPE
-echo ::set-output name=branch::$OSTYPE
+echo ::set-output name=file::$file
