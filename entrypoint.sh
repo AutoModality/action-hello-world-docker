74fdaea3ba5d21eacf714e9f518b82912aba9bdd
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 15:47:36 -0800

    master trying out add-path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,19 @@ who_to_greet=$1
 shared_files=$2
 
 echo "docker pwd $(pwd)"
-echo "shared files $shared_files"
-ls -l $shared_files
 
 message="Hello $who_to_greet"
 
-file="$shared_files/hello-world.txt"
+
+#https://help.github.com/en/actions/automating-your-workflow-with-github-actions/development-tools-for-github-actions#add-a-system-path-add-path
+file="hello-world.txt"
+file_path="::add-path::/$file"
 echo "$message" > $file
-ls -l $file
-ls -l $shared_files
-echo $message
+
 time=$(date)
 tag=refs/tags/1.1.0-rc.3
+
+echo ::set-output name=message::$message
 echo ::set-output name=time::$time
 echo ::set-output name=os::$OSTYPE
-echo ::set-output name=file::$file
+echo ::set-output name=file::$file_path
