1334c59a8dbab88bc4db07a821aa57f3c8e773d2
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 15:59:17 -0800

    master add-path from main.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 who_to_greet=$1
-shared_files=$2
+staging_dir=$2
 
 echo "docker pwd $(pwd)"
 
@@ -10,8 +10,10 @@ message="Hello $who_to_greet"
 
 #https://help.github.com/en/actions/automating-your-workflow-with-github-actions/development-tools-for-github-actions#add-a-system-path-add-path
 file="hello-world.txt"
-file_path="::add-path::/$file"
+file_path="$staging_dir/$file"
 echo "$message" > $file
+
+echo "Saving $message to $file_path"
 
 time=$(date)
 tag=refs/tags/1.1.0-rc.3
