daea0919c9a5e839bc79249ed276467fbd0f05fb
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 13:29:24 -0800

    master added workspace debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh -l
 
 who_to_greet=$1
+working_directory=$2
+
+echo "docker pwd $(pwd)"
+echo "workflow workspace $working_directory"
 
 echo "Hello $who_to_greet"
 time=$(date)
+tag=refs/tags/1.1.0-rc.3
 echo ::set-output name=time::$time
 echo ::set-output name=os::$OSTYPE
+echo ::set-output name=tag::$OSTYPE
+echo ::set-output name=branch::$OSTYPE
