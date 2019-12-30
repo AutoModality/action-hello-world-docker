977da0b221147fc4996563fbd8cdb05d87fd8473
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 20:22:29 -0800

    master using cryptic path found from docker mapping

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,22 @@
 #!/bin/sh -l
 
 who_to_greet=$1
-staging_dir=$2
-
-echo "docker pwd $(pwd)"
+staging_dir=/github/home
 
 message="Hello $who_to_greet"
 
 
-#https://help.github.com/en/actions/automating-your-workflow-with-github-actions/development-tools-for-github-actions#add-a-system-path-add-path
 file="hello-world.txt"
 file_path="$staging_dir/$file"
 
 echo "Saving $message to $file_path"
 echo "$message" > $file_path
-cat $file_path
 
-ls -l $staging_dir
-
-
+workflow_file_path="/home/runner/work/_temp/_github_home/$file"
 time=$(date)
 tag=refs/tags/1.1.0-rc.3
 
 echo ::set-output name=message::$message
 echo ::set-output name=time::$time
 echo ::set-output name=os::$OSTYPE
-echo ::set-output name=file::$file_path
+echo ::set-output name=file::$workflow_file_path
