331494c89f83132d3128929bef9344005d61c97d
Aaron Roller <aaron.roller@automodality.com>
2020-01-12 20:20:54 -0800

    save to file by sha other references need tag to avoid downloading new BB-790/docker-cache

--- .github/docker-cache.sh
+++ .github/docker-cache.sh
@@ -3,23 +3,23 @@
 # given the directory that the Github Actions cache will use, this will load the image from cache making it available
 
 # see BB-790 
-cache_dir=$1
+cache_dir=${1-:$(mktemp)}
 docker_image=$(cat .github/docker-image.txt)
 docker_version=$(cat .github/docker-version.txt)
 
-if [[ -n $cache_dir ]]; then
-    echo "usage: ./docker-cache.sh {path to cache dir}"
-fi
 archive_file=$cache_dir/cache.tar.gz
 
 echo "Docker Image = $docker_image -> $docker_version"
 
 # pull and save the image to the cache file
-if [[ ! -f "$FILE" ]]; then
+if [[ ! -f "$archive_file" ]]; then
     mkdir -p $cache_dir
     docker pull $docker_version
-    docker save --output $archive_file $docker_image
+    echo "Saving $docker_image to $archive_file"
+    docker save --output $archive_file $docker_version
 fi
+
+echo "Loading $docker_image from $archive_file"
 
 # always load the cache file so the image is available
 docker load --input $archive_file
